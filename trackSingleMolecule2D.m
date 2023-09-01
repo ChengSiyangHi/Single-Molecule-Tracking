e958838ee8408878d4751d60f31e19dc87dbe211
Siyang Cheng <143525650+ChengSiyangHi@users.noreply.github.com>
2023-09-01 16:12:01 -0500

    Add files via upload

--- trackSingleMolecule2D.m
+++ trackSingleMolecule2D.m
@@ -1,5 +1,5 @@
 %% trackSingleMolecule2D
-% Last modified on August 28, 2023 by Siyang Cheng (sc201@rice.edu)
+% Last modified on September 1, 2023 by Siyang Cheng (sc201@rice.edu)
 
 clear
 
@@ -63,10 +63,44 @@ for i = 1:lastlabel % reassemble beadlabel into a structured array 'tracks' cont
     tracks(i).frame = frame(moli);
 end
 
+%% Link trajectories between blank frames (Optional section)
+% frame_blank = 5; % threshold of blank frames
+% blank_radius = micron_search_radius*frame_blank; % threshold of identifying the same molecule after blank frames
+% 
+% trackslinked=tracks;
+% 
+% % Might connect more than 2 trajectories
+% for i=1:lastlabel-1
+%     for j=i+1:lastlabel
+%         if (trackslinked(j).frame(1)-trackslinked(i).frame(length(tracks(i).frame)))>0 && (trackslinked(j).frame(1)-trackslinked(i).frame(length(tracks(i).frame)))<blank_radius
+%             dx=trackslinked(i).x(length(tracks(i).x))-trackslinked(j).x(1);
+%             dy=trackslinked(i).y(length(tracks(i).y))-trackslinked(j).y(1);
+%             dr2=dx^2+dy^2;
+%             if dr2<blank_radius^2
+%                 trackslinked(i).x=[trackslinked(i).x trackslinked(j).x];
+%                 trackslinked(i).y=[trackslinked(i).y trackslinked(j).y];
+%                 trackslinked(i).frame=[trackslinked(i).frame trackslinked(j).frame];
+%                 trackslinked(j).x=0;trackslinked(j).y=0;trackslinked(j).frame=0;
+%                 % trackslinked(j)=[];
+%             end
+%         end
+% 
+%     end
+% 
+% end
+% 
+% count=0;
+% for i=1:lastlabel
+%     if trackslinked(i).x(1)==0
+%         count=count+1;
+%     end
+% end
+% 
+% disp([num2str(count),' trajectories combined.');
 %% Output
 % Histogram of trajectory length
 trajLength=plotTrajLength(tracks,lastlabel);
-h = histogram(trajLength, 'Normalization', 'pdf');% Normoalized histogram
+figure;h=histogram(trajLength, 'Normalization', 'pdf');% Normoalized histogram
 
 
 min_track_length=10; % threshold of trajectory length
