d18e89c09782d0d4e5877edaaab85e0e49627534
Siyang Cheng <143525650+ChengSiyangHi@users.noreply.github.com>
2023-09-05 17:01:17 -0500

    Delete plotTrajLength.m
    Renamed functions as 'calc'

--- plotTrajLength.m
+++
@@ -1,8 +0,0 @@
-function trajLength=plotTrajLength(tracks,lastlabel)
-    % colors=prism(lastlabel);
-    trajLength=zeros(1,lastlabel);
-    for i=1:lastlabel
-        trajLength(i)=length(tracks(i).frame);
-    end
-
-end
